1482b9a42457c18de659e052302421a22cbe5b64
serendipity527 <347699233@qq.com>
2025-10-28 22:48:19 +0800

    - init

---
+++ src/test/java/com/yihu/agent/HealthCareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yihu.agent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthCareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
